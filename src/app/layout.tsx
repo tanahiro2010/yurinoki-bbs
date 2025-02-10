53777f369a3af7dd8c928d9c03777d251e31be9a
tanahiro2010 <herentongkegu087@gmail.com>
2025-02-10 20:08:48 +0900

    Edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Toaster } from "@/components/ui/sonner";
 import "@/styles/globals.css";
-export const runtime = 'edge';
 
 export const metadata: Metadata = {
   title: {
--- src/app/thread/[threadId]/page.tsx
+++ src/app/thread/[threadId]/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-export const runtime = 'edge';
 
 import { useEffect, useState, useRef, Ref } from "react";
 import { useParams } from "next/navigation";
