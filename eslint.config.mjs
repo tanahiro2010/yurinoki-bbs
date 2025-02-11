2748a16b160e26b43f20a31d7327c8f110d8a1d5
tanahiro2010 <herentongkegu087@gmail.com>
2025-02-11 11:17:35 +0900

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-async-client-component": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@next/next/no-sync-scripts": "off"
+    },
+  }
 ];
-
-export default eslintConfig;
