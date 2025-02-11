4512effa0eb3f597c16fabb0ccef9e236cea3da8
tanahiro2010 <herentongkegu087@gmail.com>
2025-02-11 12:57:41 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,6 @@ export default function RootLayout({
 
         <Toaster />
         <script src="//cdn.tailwindcss.com"></script>
-        <div className="text-center mt-5">
-          <script src="https://adm.shinobi.jp/s/4aebe568090b6be9c4c1a7ff6b40bff1"></script>
-        </div>
       </body>
     </html>
   );
