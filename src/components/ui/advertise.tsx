c965e6ed879013076111789e6d079a36a7b79477
tanahiro2010 <herentongkegu087@gmail.com>
2025-02-11 12:59:14 +0900

    Create advertise.tsx

---
+++ src/components/ui/advertise.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { ReactElement } from "react"; 
+
+export default function Advertise(): ReactElement {
+  return (
+    <div className="text-center mt-5">
+      <div className="text-center mt-5">
+        <script src="https://adm.shinobi.jp/s/4aebe568090b6be9c4c1a7ff6b40bff1"></script>
+      </div>
+    </div>
+  );
+}
